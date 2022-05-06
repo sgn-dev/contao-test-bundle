26737014a2146be5dfe46ff8bebb587c11974e00
Holger Neuner <neuner@47gradnord.de>
2022-05-06 15:16:33 +0200

    dev

--- src/DependencyInjection/ContaoTestExtension.php
+++ src/DependencyInjection/ContaoTestExtension.php
@@ -10,14 +10,14 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
-namespace Contao\SkeletonBundle\DependencyInjection;
+namespace SgnDev\ContaoTestBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class ContaoSkeletonExtension extends Extension
+class ContaoTestExtension extends Extension
 {
     public function load(array $mergedConfig, ContainerBuilder $container): void
     {
